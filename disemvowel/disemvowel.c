e7b4009d68089e9469c6b8539fdecd6c4c9a7cf5
rabex028 <rabex028@morris.umn.edu>
2018-09-22 16:10:56 -0500

    Fix function imports and scoping issues
    Swapped the order of helper functions in
    `disemvowel.c` so that they are recognizable
    in the right order for the compiler. Included
    imports at the top of `disemvowel.c` to make
    strlen and tolower functions compilable. `main.c`
    will now compile without errors, but has warnings.

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,34 +1,42 @@
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 #include "disemvowel.h"
 
 char *disemvowel(char *str) {
-  int count = 0;
-
-  for(int i = 0; i < strlen(str); i++) {
-  	if(str[i] != isVowel(str[i]) {
-		count++;
-	} 
-  }
 
   return (char*) "";
 }
 
 bool isVowel(char letter) {
-	letter = tolower(letter);
-	switch(letter) {
-		case 'a':
-			return true;
-		case 'e':
-			return true;
-		case 'i':
-			return true;
-		case 'o':
-			return true;
-		case 'u':
-			return true;
-		default :
-			return false;	
+        letter = tolower(letter);
+        switch(letter) {
+                case 'a':
+                        return true;
+                case 'e':
+                        return true;
+                case 'i':
+                        return true;
+                case 'o':
+                        return true;
+                case 'u':
+                        return true;
+                default :
+                        return false;
 
-	}
+        }
 }
+
+
+int allocateSpace(char *str) {
+int count = 0;
+
+for(int i = 1; i < strlen(str); i++) {
+	if (str[i] != isVowel(str[i])) {
+		count++;
+		}
+	}
+	return count;
+}
+
